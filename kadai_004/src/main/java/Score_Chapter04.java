3959b79ae2227d83948110ea40ed11b478cfc6a1
ohata yukino <140718632+yukikietuki@users.noreply.github.com>
2023-08-20 15:12:51 +0900

    Add files via upload

---
+++ kadai_004/src/main/java/Score_Chapter04.java
@@ -0,0 +1,16 @@
+public class Score_Chapter04 {
+    public static void main(String[] args){
+        System.out.println("Aさん" + 10 + "点");
+        System.out.println("Bさん" + 20 + "点");
+        System.out.println("Cさん" + 30 + "点");
+        System.out.println("Dさん" + 40 + "点");
+        System.out.println("Eさん" + 50 + "点");
+        System.out.println("Fさん" + 60 + "点");
+        System.out.println("Gさん" + 70 + "点");
+        System.out.println("Hさん" + 80 + "点");
+        System.out.println("Iさん" + 90 + "点");
+        System.out.println("Jさん" + 100 + "点");
+        System.out.println("平均点" + ((10+20+30+40+50+60+70+80+90+100)/10 )+ "点");
+
+    }
+}
